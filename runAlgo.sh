8bd7d59b887d842b64e12f600170a4320bd236cc
bcreagh <briancreagh95@gmail.com>
2019-04-23 19:05:20 +0100

    Accept cli args for runAlgo.sh

--- runAlgo.sh
+++ runAlgo.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-# This runs the project simply using the java executable
+usage() {
+    echo "Usage:"
+    echo "runAlgo.sh <Class-name of algorithm>"
+}
+
+if [[ $1 == "-h" ]]; then
+  usage
+  exit 0
+fi
+
+if [[ -z $1 ]]; then
+  echo "You must specify an algorithm to run!"
+  usage
+  exit 1
+fi
 
 mvn compile
 java -cp "target/classes" com.bcreagh.javaalgos.Reverse
