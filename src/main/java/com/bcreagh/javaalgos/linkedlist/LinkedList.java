781ee94419d3ee978c29ef9fff27fb309a403864
bcreagh <briancreagh95@gmail.com>
2019-05-20 20:21:37 +0100

    Add linked list implementation

---
+++ src/main/java/com/bcreagh/javaalgos/linkedlist/LinkedList.java
@@ -0,0 +1,111 @@
+package com.bcreagh.javaalgos.linkedlist;
+
+public class LinkedList<T> {
+    private int length = 0;
+    private Node<T> first;
+    private Node<T> last;
+
+    // O(n)
+    // O(1) for adding at position 0
+    public void addAt(int position, T item) {
+        if (position < 0 || position > length) {
+            throw new IndexOutOfBoundsException(
+                    String.format("The length of the list is %d, the index you provided was %d", length, position));
+        }
+        Node<T> newNode = new Node<>(item);
+        if (position == 0) {
+            newNode.next = first;
+            first = newNode;
+        } else {
+            Node<T> previousNode = getNodeAt(position -1);
+            newNode.next = previousNode.next;
+            previousNode.next = newNode;
+        }
+        if (position == length) {
+            last = newNode;
+        }
+        length += 1;
+    }
+
+    // O(1)
+    public void add(T item) {
+        Node<T> newNode = new Node<>(item);
+        Node<T> oldLast = last;
+        last = newNode;
+        int oldLength = length;
+        length += 1;
+        if (oldLength == 0) {
+            first = newNode;
+            return;
+        }
+        oldLast.next = newNode;
+    }
+
+    // O(n)
+    public T getAt(int position) {
+        Node<T> node = getNodeAt(position);
+        return node.item;
+    }
+
+    private Node<T> getNodeAt(int position) {
+        if (position >= length || position < 0) {
+            throw new IndexOutOfBoundsException(
+                    String.format("The length of the list is %d, the index you provided was %d", length, position));
+        }
+        Node<T> current = first;
+        for (int i = 0; i < position; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    // O(1)
+    public T getLast() {
+        if (length == 0) {
+            throw new IndexOutOfBoundsException("You cannot get the last item because the list is empty!");
+        }
+        return last.item;
+    }
+
+    // O(n)
+    // O(1) for removing at position 0
+    public T removeAt(int position) {
+        if (position >= length || position < 0) {
+            throw new IndexOutOfBoundsException(
+                    String.format("The length of the list is %d, the index you provided was %d", length, position));
+        }
+        Node<T> nodeToRemove;
+        if (position == 0) {
+            nodeToRemove = first;
+            if (length == 1) {
+                first = null;
+                last = null;
+            } else {
+                first = first.next;
+            }
+        } else {
+            Node<T> previousNode = getNodeAt(position - 1);
+            nodeToRemove = previousNode.next;
+            previousNode.next = nodeToRemove.next;
+            if (position == length - 1)  {
+                last = previousNode;
+            }
+        }
+        length -= 1;
+        return nodeToRemove.item;
+    }
+
+    // O(1)
+    public int getLength() {
+        return length;
+    }
+
+    private class Node<T> {
+        Node<T> next;
+        T item;
+
+        Node(T item) {
+            this.item = item;
+        }
+    }
+}
---
+++ src/test/java/com/bcreagh/javaalgos/linkedlist/LinkedListTest.java
@@ -0,0 +1,195 @@
+package com.bcreagh.javaalgos.linkedlist;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LinkedListTest {
+
+    LinkedList<Integer> list;
+
+    @Before
+    public void setup() {
+        this.list = new LinkedList<>();
+    }
+
+    @Test
+    public void initialLength_shouldBe0() {
+        assertEquals(0, list.getLength());
+    }
+
+    @Test
+    public void add_shouldIncreaseLengthByOne() {
+        list.add(424);
+        assertEquals(1, list.getLength());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getAt_shouldThrowException_whenPositionIsOutOfBounds() {
+        list.getAt(34);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getAt_shouldThrowException_whenPositionIsNegative() {
+        list.getAt(-1);
+    }
+
+    @Test
+    public void getAt_shouldReturnCorrectItem() {
+        int[] itemsToAdd = {124, 235, 3, 345};
+        for (int item: itemsToAdd){
+            list.add(item);
+        }
+        for (int i = 0; i < itemsToAdd.length; i++) {
+            assertTrue(itemsToAdd[i] == list.getAt(i));
+        }
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void addAt_shouldThrowExceptionWhen_positionIsOutOfBounds() {
+        int num = 32;
+        list.addAt(1, num);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void addAt_shouldThrowExceptionWhen_positionIsNegative() {
+        int num = 32;
+        list.addAt(-5, num);
+    }
+
+    @Test
+    public void addAt_shouldAddAnItem_whenPositionIs0AndListIsEmpty() {
+        int num = 32;
+        list.addAt(0, num);
+        assertEquals(num, (int) list.getAt(0));
+    }
+
+    @Test
+    public void addAt_shouldCauseLastToPointToNewItem_whenPositionIs0AndListIsEmpty() {
+        int num = 32;
+        list.addAt(0, num);
+        assertEquals(num, (int) list.getLast());
+    }
+
+    @Test
+    public void addAt_shouldLeaveItemsInListWithCorrectPositions() {
+        int originalFirst = 124;
+        int originalSecond = 2354;
+        int originalThird = 322;
+        int originalFourth = 888;
+        int newItem = 44;
+        int[] itemsToAdd = {originalFirst, originalSecond, originalThird, originalFourth};
+        for (int item: itemsToAdd){
+            list.add(item);
+        }
+        list.addAt(1, newItem);
+        assertEquals(originalFirst, (int) list.getAt(0));
+        assertEquals(newItem, (int) list.getAt(1));
+        assertEquals(originalSecond, (int) list.getAt(2));
+        assertEquals(originalThird, (int) list.getAt(3));
+        assertEquals(originalFourth, (int) list.getAt(4));
+    }
+
+    @Test
+    public void addAt_shouldCauseLengthToIncreaseByOne() {
+        int originalLen = list.getLength();
+        list.addAt(0, 32);
+        assertEquals(originalLen + 1, list.getLength());
+    }
+
+    @Test
+    public void getLast_shouldReturnCorrectItem_afterAddAtIsPerformed() {
+        int lastItem = 243;
+        list.add(lastItem);
+        list.addAt(0, 32);
+        list.addAt(1, 989);
+        assertEquals(lastItem, (int) list.getLast());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void removeAt_shouldThrowException_whenPositionIsOutOfBounds() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.removeAt(10);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void removeAt_shouldThrowException_whenPositionIsNegative() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.removeAt(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void removeAt_shouldThrowException_whenPosition0AndListIsEmpty() {
+        list.removeAt(0);
+    }
+
+    @Test
+    public void removeAt_shouldReturnCorrectItem() {
+        int itemAtPosition2 = 9236;
+        list.add(12);
+        list.add(8732);
+        list.add(itemAtPosition2);
+        list.add(5453);
+        assertEquals(itemAtPosition2, (int) list.removeAt(2));
+    }
+
+    @Test
+    public void removeAt_shouldDecreaseLengthByOne() {
+        list.add(12);
+        list.add(8732);
+        list.add(5453);
+        assertEquals(3, list.getLength());
+        list.removeAt(2);
+        assertEquals(2, list.getLength());
+    }
+
+    @Test
+    public void removeAt_shouldSetNextNodeToSpecifiedPosition() {
+        int itemAtOriginallyPosition2 = 23876;
+        int itemAtOriginallyPosition3 = 3223;
+        int[] items = {32, 1, itemAtOriginallyPosition2, itemAtOriginallyPosition3, 65};
+        for (int item: items) {
+            list.add(item);
+        }
+        assertEquals(itemAtOriginallyPosition2, (int) list.getAt(2));
+        list.removeAt(2);
+        assertEquals(itemAtOriginallyPosition3, (int) list.getAt(2));
+    }
+
+    @Test
+    public void removeAt_shouldCorrectlySetFirst_whenPositionIs0() {
+        int itemOriginallyAtPosition1 = 123;
+        int[] items = {32, itemOriginallyAtPosition1, 234, 33, 65};
+        for (int item: items) {
+            list.add(item);
+        }
+        list.removeAt(0);
+        assertEquals(itemOriginallyAtPosition1, (int) list.getAt(0));
+    }
+
+    @Test
+    public void removeAt_shouldCorrectlySetLast_whenRemovingLastItem() {
+        int originalSecondLastItem = 123;
+        int[] items = {32, 23, 234, originalSecondLastItem, 65};
+        for (int item: items) {
+            list.add(item);
+        }
+        list.removeAt(list.getLength() - 1);
+        assertEquals(originalSecondLastItem, (int) list.getLast());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void removeAt_shouldCauseSubsequentGetLastToThrowException_whenPositionIs0AndLengthIs1() {
+        System.out.println("Len: " + list.getLength());
+        list.add(123);
+        System.out.println("Len: " + list.getLength());
+        list.removeAt(0);
+        System.out.println("Len: " + list.getLength());
+        assertNull(list.getLast());
+    }
+
+}
